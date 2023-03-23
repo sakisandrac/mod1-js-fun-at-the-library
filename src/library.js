28c01ab18940f3f3a2ded80819fc1bce2049c8ed
“Saki <sakisandra.c@gmail.com>
2023-03-22 20:04:52 -0600

    Create passing functions for createLibrary and addBook

--- src/library.js
+++ src/library.js
@@ -1,8 +1,30 @@
+function createLibrary(name){
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library;
+}
+
+function addBook(library, book){
+  if(book.genre === 'fantasy'){
+    library.shelves.fantasy.push(book)
+  } else if(book.genre === 'fiction'){
+    ibrary.shelves.fiction.push(book)
+  } else if(book.genre === 'nonFiction'){
+    library.shelves.nonFiction.push(book)
+  }
+}
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
