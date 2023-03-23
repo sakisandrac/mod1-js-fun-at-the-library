71b18a3e7e134c2fd3e61695ff9558be032c54ab
“Saki <sakisandra.c@gmail.com>
2023-03-23 14:12:42 -0600

    Create listTitles function

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,6 @@
 function shelfBook(book, shelf){
   if (shelf.length !== 3){
-  shelf.unshift(book)
+  shelf.unshift(book);
   }
 }
 
@@ -8,14 +8,33 @@ function unshelfBook(name, shelf){
   for (var i=0; i < shelf.length; i++)
   if (shelf[i].title.includes(name) && name === shelf[i].title){
     shelf.splice(i, 1);
-    return shelf
+    return shelf;
   }
  }
 
+//  function listTitles(shelf){
+//   for (var i=0; i < shelf.length; i++){  
+//     shelf[i].title
+//   } 
+// }
+
+function listTitles(shelf){
+  var titles = shelf.map(function(book){
+    return book.title
+  })
+  return titles.join(", ");
+}
+
+function searchShelf(shelf, title){
+  for (var i=0; i < shelf.length; i++)
+  if (shelf[i].title.includes(title)){
+    return true
+  } return false
+}
 
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
